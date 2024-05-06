f6cb0b717d5ecacb31cf88b47c70307bc9fe9cb0
nokibh <nokibhowlader2003@gmail.com>
2024-05-06 23:01:23 +0600

    added new

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,15 @@
+import About from '../About/About';
 import Banner from '../Banner/Banner';
+import Servises from '../Servises/Servises';
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <div className="mb-4 mt-4">
+        <About></About>
+      </div>
+      <Servises></Servises>
     </div>
   );
 };
---
+++ src/Pages/Servises/Servises.jsx
@@ -0,0 +1,17 @@
+const Servises = () => {
+  return (
+    <div>
+      <div className="text-center space-y-2 mt-4 mb-4">
+        <h3 className="text-[#FF3811] font-bold">Service</h3>
+        <h2 className="text-black font-bold">Our Service Area</h2>
+        <p>
+          the majority have suffered alteration in some form, by injected
+          humour, <br /> or randomised words which do not look even slightly
+          believable.{' '}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Servises;
